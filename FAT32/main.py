22dceb09f0e260e404ddfadbfc56946c8974e3bb
vhakHCMUS <22127201@student.hcmus.edu.vn>
2024-03-06 21:12:19 +0700

    FAT32 Khoa

--- FAT32/main.py
+++ FAT32/main.py
@@ -1,6 +1,30 @@
 import os
-from FAT32 import *
 
+#read from sectorStart to sectorStart + numberSector return  number of bytes 
+def readSectors(file, sectorStart, numberSector, bytesPersector = 512):
+    file.seek(sectorStart * bytesPersector)
+    return file.read(numberSector * bytesPersector)
+
+#
+def readNumBuffer(buffer, offset, size):
+    # Kiểm tra nếu offset không có tiền tố "0x", thêm tiền tố "0x"
+    if not offset.startswith("0x"):
+        offset = "0x" + offset
+    
+    # Chuyển offset thành số nguyên
+    offset_int = int(offset, 16)
+    
+    # Lấy phần của buffer từ offset đến offset + size
+    sliced_buffer = buffer[offset_int: offset_int + size]
+    
+    # Đảo ngược thứ tự của mảng buffer
+    reversed_buffer = sliced_buffer[::-1]
+    
+    # Chuyển đổi mảng reversed_buffer thành số nguyên
+    result = int.from_bytes(reversed_buffer, byteorder='big')
+    
+    return result
+   
 def intToAscii(value):
     try:
         ascii_string = value.to_bytes((value.bit_length() + 7) // 8, byteorder='big').decode('ascii')
